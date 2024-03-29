10c168e6bf211611f0c30ce06fa55a8feca5d792
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-03-29 12:25:37 -0700

    Esboço de testes unitários.

---
+++ testes/bibliotecas/matematica.test.ts
@@ -0,0 +1,64 @@
+import { potencia } from '../../fontes/bibliotecas/matematica';
+
+describe('Biblioteca Matemática', () => {
+    describe('Potência', () => {
+        it('Trivial', async () => {
+            const resultado = await potencia(2, 8);
+            expect(resultado).toBe(256);
+        });
+    });
+
+    describe('Raiz', () => {
+        it('Trivial', async () => {
+
+        });
+    });
+
+    describe('Arredondar', () => {
+        it('Trivial', async () => {
+
+        });
+    });
+
+    describe('Logaritmo', () => {
+        it('Trivial', async () => {
+
+        });
+    });
+
+    describe('Seno', () => {
+        it('Trivial', async () => {
+
+        });
+    });
+
+    describe('Cosseno', () => {
+        it('Trivial', async () => {
+
+        });
+    });
+
+    describe('Tangente', () => {
+        it('Trivial', async () => {
+
+        });
+    });
+
+    describe('Valor Absoluto', () => {
+        it('Trivial', async () => {
+
+        });
+    });
+
+    describe('Maior Número', () => {
+        it('Trivial', async () => {
+
+        });
+    });
+
+    describe('Menor Número', () => {
+        it('Trivial', async () => {
+
+        });
+    });
+});
